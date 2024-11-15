7686895f190f966d6a9855b5c1557440418bc6c3
Vanya705 <vanya705@gmail.com>
2024-11-16 01:37:15 +0400

    vite

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react({ jsxRuntime: 'classic' }), svgr()],
+  plugins: [react(), svgr()],
    base: "/movies-react/"
 })
