f05fa4e67c2141ac61e81c97f3e7b9f4c0c104f9
Vanya705 <vanya705@gmail.com>
2024-11-16 01:29:56 +0400

    sd

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr()],
+  plugins: [react({ jsxImportSource: '@emotion/react' }), svgr()],
    base: "/movies-react/"
 })
