a77c4a790674a229a5d2f16a06abbac0eaa398ca
Vanya705 <vanya705@gmail.com>
2024-11-16 23:30:27 +0400

    s

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import svgr from 'vite-plugin-svgr';
-import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
-  build: {
-    outDir: 'dist',
-    rollupOptions: {
-      input: {
-        // eslint-disable-next-line no-undef
-        main: resolve(__dirname, 'index.html'),
-      },
-    },
-  },
+  base: "/movies-react/",
   server: {
     historyApiFallback: true,
   },
